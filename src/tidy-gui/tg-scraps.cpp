1e00191325694ef3f732d8a9e418b1d674a15613
Geoff McLane <ubuntu@geoffair.info>
2015-05-29 10:54:55 +0200

    Add module to hold some code scraps

---
+++ src/tidy-gui/tg-scraps.cpp
@@ -0,0 +1,17 @@
+/*\
+ * tg-scraps.cpp
+ *
+ * Copyright (c) 2015 - Geoff R. McLane
+ * Licence: GNU GPL version 2
+ *
+\*/
+
+#include <stdio.h>
+// other includes
+
+static const char *module = "tg-scraps";
+
+const char *get_tg_scraps_mod_name() { return module; }
+
+
+// eof = tg-scraps.cpp
