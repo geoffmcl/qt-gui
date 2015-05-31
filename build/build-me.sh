6ec1a85e47faf9539ea9827f1fd6d1f6241b4356
Geoff R. McLane <ubuntu@geoffair.info>
2015-05-31 15:49:58 +0200

    add options to build-me.sh

--- build/build-me.sh
+++ build/build-me.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-#< build-me.sh - qt-gui - 20150524
+#< build-me.sh - qt-gui - 20150531 - 20150524
+# 20150531 - add some command options
 BN=`basename $0`
 TMPPRJ="qt-gui"
 TMPLOG="bldlog-1.txt"
@@ -8,9 +9,69 @@ TMPOPTS="-DCMAKE_INSTALL_PREFIX=$HOME"
 # Possible options to help with problems
 # TMPOPTS="$TMPOPTS -DCMAKE_VERBOSE_MAKEFILE=ON"
 
+VERBOSE=0
+DBGSYMS=0
+DOPAUSE=0
+
+# option like EXTRA=-DCMAKE_BUILD_TYPE=DEBUG
+add_extra_cmopt()
+{   
+    if [ "$#" -gt "0" ]; then
+        LEN1=`expr length $1`
+        if [ "$LEN1" -gt "7" ]; then
+            XOPT=`echo $1 | cut -b7-$LEN1`
+            TMPOPTS="$TMPOPTS $XOPT"
+            echo "$BN: Add EXTRA option [$XOPT]"
+        else
+            echo "$BN: ERROR: Length $LEN! less than/equals 6"
+            exit 1
+        fi
+    else
+        echo "$BN: ERROR: No option passed!"
+        exit 1
+    fi
+}
+
+give_help()
+{
+    echo "$BN [OPTIONS]"
+    echo ""
+    echo "OPTIONS"
+    echo " VERBOSE = Use verbose build (def=$VERBOSE)"
+    echo " DEBUG   = Enable DEBUG symbols (-g)."
+    echo " EXTRA=CMOPT = Add a extra CMake option."
+#    echo " NOPAUSE = Skip the pausing before each step."
+    #echo " PROFILING = Enable PROFILING (-pg)."
+    echo ""
+    exit 1
+}
+
+
 for arg in $@; do
-	TMPOPTS="$TMPOPTS $arg"
+      case $arg in
+         VERBOSE) VERBOSE=1 ;;
+         DEBUG) DBGSYMS=1 ;;
+#         NOPAUSE) DOPAUSE=0 ;;
+         EXTRA=*) add_extra_cmopt $arg ;;
+         --help) give_help ;;
+         -h) give_help ;;
+         -\?) give_help ;;
+         *)
+            echo "$BN: ERROR: Invalid argument [$arg]"
+            exit 1
+            ;;
+      esac
 done
+
+if [ "$VERBOSE" = "1" ]; then
+    TMPOPTS="$TMPOPTS -DCMAKE_VERBOSE_MAKEFILE=TRUE"
+    echo "$BN: Enabling VERBOSE make"
+fi
+
+if [ "$DBGSYMS" = "1" ]; then
+    TMPOPTS="$TMPOPTS -DCMAKE_BUILD_TYPE=Debug -DENABLE_DEBUG_SYMBOLS:BOOL=TRUE"
+    echo "$BN: Enabling DEBUG symbols"
+fi
 
 if [ -f "$TMPLOG" ]; then
     rm -f $TMPLOG
